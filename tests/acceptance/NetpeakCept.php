d3318bc48985b86aebbc6252e3d1c709237889f5
oleksii <oleksii.trufanov@gmail.com>
2022-01-30 14:31:36 +0300

    first

---
+++ tests/acceptance/NetpeakCept.php
@@ -0,0 +1,49 @@
+<?php
+use Codeception\Util\Locator;
+use Codeception\Module\Asserts;
+
+$I = new AcceptanceTester($scenario);
+$I->wantTo('see start page, go to Career page, interact with resume page and go to Courses page');
+$I->amOnPage('/');
+
+//Check button "Career" on the page and click
+$I->see('Карьера','#allrecords a.custom-link-black');
+$I->click(Locator::contains('#allrecords a.custom-link-black', 'Карьера'));
+$I->amOnUrl('https://career.netpeak.group/');
+
+//Check button "I want to work" on the page and click
+$I->see('Я хочу работать в Netpeak', '/html/body/div[5]/div/div/div[2]/div/a');
+$I->click(Locator::contains('/html/body/div[5]/div/div/div[2]/div/a','Я хочу работать в Netpeak'));
+$I->amOnUrl('https://career.netpeak.group/hiring/');
+
+//Check button "Upload Resume" on the page, attach wrong file test.png
+//and see that error message appeared
+$I->see('Загрузить резюме', '#upload');
+$I->attachFile("input[type='file']", 'test.png');
+$I->wait(5);
+$I->see('Ошибка: данные файла не были переданы.', '#up_file_name label.control-label');
+
+//Create Random variables for Name, LastName and Email
+$randomName = $I->executeJS("return Math.random().toString(36).substring(2,12);");
+$randomLastname = $I->executeJS("return Math.random().toString(36).substring(2,12);");
+$randomEmail = $I->executeJS("var chars = 'abcdefghijklmnopqrstuvwxyz'; return chars[Math.floor(Math.random()*26)] + Math.random().toString(36).substring(2,11) + '@domain.com';");
+
+//Fill fields in "personal data" block using random variables and press button "submit" resume
+$I->fillField('#inputName', "$randomName");
+$I->fillField('#inputLastname', "$randomLastname");
+$I->fillField('#inputEmail', "$randomEmail");
+$I->fillField('#inputPhone', "+380938003287");
+$I->selectOption('form select[name=by]','1969');
+$I->selectOption('form select[name=bm]','12');
+$I->selectOption('form select[name=bd]','25');
+$I->click(Locator::contains('#submit','Отправить анкету'));
+$I->wait(3);
+
+// Check color of warning fields changed to red
+$color = $I->executeJS("return jQuery('div p.warning-fields.help-block').css('color');");
+$I->assertEquals( $color, 'rgb(255, 0, 0)' );
+
+// Check for button "Courses", click and see that you are on "Courses" page
+$I->see('Интернатура', '#main-menu li.blog a');
+$I->click(Locator::contains('#main-menu li.blog a','Интернатура'));
+$I->amOnUrl('https://school.netpeak.group/');
