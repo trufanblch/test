6f455dfda9b7a35f2026605bf96168dbff0c711b
OleksiiTrufanov <47119760+OleksiiTrufanov@users.noreply.github.com>
2023-03-21 16:58:42 +0200

    Update NetpeakCept.php

--- tests/acceptance/NetpeakCept.php
+++ tests/acceptance/NetpeakCept.php
@@ -1,12 +1,20 @@
 <?php
 use Codeception\Util\Locator;
 use Codeception\Module\Asserts;
+//
+//
+//
+//
+//
+//
+//
+
 
 $I = new AcceptanceTester($scenario);
 $I->wantTo('see start page, go to Career page, interact with resume page and go to Courses page');
 $I->amOnPage('/');
 
-//Check button "Career" on the page and click
+//Check button "Career" on the page and click asd
 $I->see('Карьера','#allrecords a.custom-link-black');
 $I->click(Locator::contains('#allrecords a.custom-link-black', 'Карьера'));
 $I->amOnUrl('https://career.netpeak.group/');
